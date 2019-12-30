ff2e1a22109f631ba257846384048b1d9bedf54d
Khoa <dangkhoapham1983@gmail.com>
2019-12-30 15:38:31 +0700

    demo
    demo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+//const { spaceId, accessToken } = process.env;
 
 module.exports = {
-  /* Your site config here */
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: "l52ees7wpuuj",
+        accessToken: process.env.accessToken
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,42 @@
-import React from "react"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const HomePage = () => (
+  <StaticQuery
+    query={graphql`
+      query HomePage {
+        contentfulPost {
+          title
+          date
+          content {
+            content
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+        contentfulPost: {
+        title,
+        date,
+        content: { content },
+        image: {
+          file: { url }
+        }
+      }
+    }) => (
+      <>
+        <h1>{title}</h1>
+        <small>Created on {date}</small>
+        <img src={url} />
+        <p>{content}</p>
+      </>
+    )}
+  />
+);
+
+export default HomePage;
